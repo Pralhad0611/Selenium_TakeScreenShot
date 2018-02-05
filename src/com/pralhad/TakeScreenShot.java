3687db1b6124bb1819f0e07e0dc1b4be347d4910
PSP <pp@sunrichtech.com>
2018-02-05 10:02:14 +0530

    first commit

---
+++ src/com/pralhad/TakeScreenShot.java
@@ -0,0 +1,31 @@
+package com.pralhad;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import java.io.File;
+import java.io.IOException;
+
+public class TakeScreenShot
+{
+    WebDriver w;
+
+    @Test
+    public void test () throws InterruptedException, IOException
+    {
+        System.setProperty("webdriver.chrome.driver", "F:/chromedriver.exe");
+        w = new ChromeDriver();
+        w.manage().window().maximize();
+        w.navigate().to("http://localhost:9000");
+        w.manage().deleteAllCookies();
+
+        //Capture entire page screenshot and then store it to destination drive
+        File screenshot = ((TakesScreenshot)w).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("F:\\screenshot.jpg"));
+        System.out.print("Screenshot is captured");
+    }
+}
